99763c307d78770d614e981181d47a7bf24c80cf
r.noei <reza.noei@chmail.ir>
2022-06-04 18:35:10 +0430

    Add comments for copyright.

--- Linphone/Views/HttpContentExtensions.cs
+++ Linphone/Views/HttpContentExtensions.cs
@@ -1,6 +1,6 @@
 ﻿/*
 IncomingCall.xaml.cs
-Copyright (C) 2016  Belledonne Communications, Grenoble, France
+Copyright (C) 2022 Resa alongside of Belledonne Communications, Grenoble, France
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
 as published by the Free Software Foundation; either version 2
