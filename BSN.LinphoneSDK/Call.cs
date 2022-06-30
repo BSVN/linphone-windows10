d78e5f762944cf93ac065190a9d3f28d3861f344
Seyyed Soroosh Hosseinalipour <soorosh_abi@hotmail.com>
2022-06-30 23:36:05 +0430

    Add BSN.LinphoneSDK project

---
+++ BSN.LinphoneSDK/Call.cs
@@ -0,0 +1,14 @@
+﻿using Linphone;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BSN.LinphoneSDK
+{
+    public class Calls
+    {
+		public Call RawCall { get; set; }
+	}
+}
